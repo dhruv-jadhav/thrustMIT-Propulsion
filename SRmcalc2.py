fd4ec1508850f29d02ebd3409f62edc25adc656d
Dhruv Jadhav <dhruvjadhav225@gmail.com>
2024-10-22 20:06:08 +0530

    reformatted stuff

--- SRmcalc2.py
+++ SRmcalc2.py
@@ -1,57 +1,73 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#to find some key motor parameters 
+
+# to find some key motor parameters
 def constant(k, R, To, a, n, pb):
     C = ((a * pb) / np.sqrt((k / (R * To)) * ((2 / (k + 1)) ** ((k + 1) / (k - 1))))) ** (1 / (1 - n))
     return C
+
 
 def chamber_pressure(C, Kn, n):
     Pc = C * (Kn) ** (1 / (1 - n))
     return Pc
 
+
 def expansion_ratio(k, Po, Pe):
-    e = ((k+1)/2)**(1/(k-1)) * (Pe/Po)**(1/k) * (((k+1)/(k-1)) * (1 - (Pe/Po)**((k-1)/k)))**(1/2)
-    E = 1/e
+    e = ((k + 1) / 2) ** (1 / (k - 1)) * (Pe / Po) ** (1 / k) * (
+                ((k + 1) / (k - 1)) * (1 - (Pe / Po) ** ((k - 1) / k))) ** (1 / 2)
+    E = 1 / e
     return E
+
 
 def throat_area(Ae, E):
     At = Ae / E
     return At
 
+
 def thrust(k, Po, Pe, Ae, At):
-    F = At * Po * np.sqrt(((2 * k**2) / (k - 1)) * (2 / (k + 1))**((k + 1) / (k - 1)) * (1 - (Pe / Po)**((k - 1) / k))) + (Pe - Po) * Ae
+    F = At * Po * np.sqrt(
+        ((2 * k ** 2) / (k - 1)) * (2 / (k + 1)) ** ((k + 1) / (k - 1)) * (1 - (Pe / Po) ** ((k - 1) / k))) + (
+                    Pe - Po) * Ae
     return F
+
 
 def Kn(Ab, At):
     Kn_value = Ab / At
     return Kn_value
 
+
 def burn_area(L, Dp, Dg, N):
-    Ab = (np.pi * Dp * L) + (N * (((np.pi) * (Dg - Dp)**2) / 4))
+    Ab = (np.pi * Dp * L) + (N * (((np.pi) * (Dg - Dp) ** 2) / 4))
     return Ab
 
+
 def burn_rate(Po, a, n):
-    r = a * (Po)**n
+    r = a * (Po) ** n
     return r
+
 
 def length_max(Dg):
     Lmax = 12 * Dg
     return Lmax
 
+
 def port_area(Dp):
-    Ap = ((np.pi) * (Dp)**2) / 4
+    Ap = ((np.pi) * (Dp) ** 2) / 4
     return Ap
+
 
 def min_Dp(At):
     Apmin = 2 * At
     Dpmin = np.sqrt((4 * Apmin) / (np.pi))
     return Dpmin
 
+
 def max_Dp(At):
     Apmax = 4 * At
     Dpmax = np.sqrt((4 * Apmax) / (np.pi))
     return Dpmax
+
 
 # Constants
 k = 1.1361
@@ -92,7 +108,7 @@ for N in np.arange(2, 5):
                         Favg += F_in
                         F_values.append(F)
                         time_list.append(t)
-                        
+
                         if np.isclose(Favg, Favg_user, atol=50):
                             print(f"Average thrust is {Favg}")
                             print(f"Length is {L}")
