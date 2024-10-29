913372a7471ef405b84f7e16416a99fdd919c67f
varun pv <beastboy313131@gmail.com>
2024-10-30 02:08:14 +0530

    oring it 3

--- ORings.py
+++ ORings.py
@@ -18,7 +18,7 @@ def dia_9(d9_base):
 
 # to find d3
 def dia_3(d3__base):
-    Tg_d3 = 87 * 10 ** (-3)
+    Tg_d3 = 87 * (10 ** (-3))
     d3_lower = d3__base
     d3_upper = d4_base + Tg_d3
     return d3_lower, d3_upper
@@ -50,14 +50,14 @@ def gland_compression(d2_lower,d2_upper):
 
 
 # Gland Fill
-def gland_fill(d2_lower,d2_upper, b):
+def gland_fill(d2_lower,d2_upper, bo):
     t_min, t_max = gland_depth()
     oring_csa_lower = math.pi * (d2_lower / 2) ** 2
     oring_csa_upper = math.pi * (d2_upper / 2) ** 2
-    gland_csa_min = t_min * b 
-    gland_csa_max = t_max * b
-    fill_percentage_min = (oring_csa / gland_csa_max) * 100
-    fill_percentage_max = (oring_csa / gland_csa_min) * 100
+    gland_csa_min = t_min * bo
+    gland_csa_max = t_max * bo
+    fill_percentage_min = (oring_csa_lower / gland_csa_max) * 100
+    fill_percentage_max = (oring_csa_upper/ gland_csa_min) * 100
     return fill_percentage_min, fill_percentage_max
 n=0
 d4_base = 95.86
@@ -75,10 +75,23 @@ while n < len(d2_base):
     d2_upper=d2_base[n]+d2_error[n]
     d3__base=d3_base[n]
     d1__base=d1_base[n]
+    bo = b[n]
     stretch_min, stretch_max = oring_stretch(d1__base)
+    print(oring_stretch(d1__base))
     compression_min, compression_max = gland_compression(d2_lower,d2_upper)
-    fill_percentage_min, fill_percentage_max = gland_fill(d2_lower,d2_upper, b):
-    if stretch_max<6 and compression_min>11 and compression_max<23 and 
+    fill_percentage_min, fill_percentage_max = gland_fill(d2_lower,d2_upper, bo)
+    if stretch_max<6 and compression_min>11 and compression_max<23 and fill_percentage_min>60 and fill_percentage_max<90:
+        print("O-ring min strech is")+print(oring_stretch(0))
+        print("O-ring max strech is")+print(oring_stretch(1))
+        print("gland depth min is")+print(gland_depth(0))
+        print("gland depth max is")+print(gland_depth(1))
+        print("O-ring compression min is")+print(gland_compression(0))
+        print("O-ring compression max is")+print(gland_compression(0))
+        print("gland fill min is")+print(gland_fill(0))
+        print("gland fill max is")+print(gland_fill(1))
+        break
+    n=n+1
+
 
 
 
